48f6ee222bf850806bdf1514627b897339971d6d
Amal Shibu <66511659+TheAmalShibu@users.noreply.github.com>
2024-01-22 11:30:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Thankyou, That made me Happy!');
+  res.send('Restart MiG Test - 1');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
